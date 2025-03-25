36e1017970d3b9b9a9a508806a6abb6a9af88d06
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-03-25 12:40:26 +0530

    refactor: replace App.css with App.module.css and update layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import style from './App.module.css'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Navbar />
-    </>
+    <div className={style["appOuterContainer"]}>
+      <div className={style["appInnerContainer"]}>
+        <Navbar />
+      </div>
+    </div>
   )
 }
 
